1b2fdc173f00b1bd88cfc940443a06531c21843a
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-08-13 17:06:17 -0500

    Set up testing file

--- test_app.py
+++ test_app.py
@@ -2,3 +2,10 @@ from flask import request
 from unittest import TestCase
 from app import app
 from convert import convert
+
+
+app.config['TESTING'] = True
+# how does the following work?
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
+
