e99fa2940b4e01c1f3c2eb090a10fc41b68d1b64
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-08-13 17:11:06 -0500

    Add tests

--- test_app.py
+++ test_app.py
@@ -13,3 +13,38 @@ class ConvertCurrencyTestCase(TestCase):
     """Unit tests on forex-python currency conversion"""
 
     def test_show_convert_form(self):
+        """Test"""
+        with app.test_client() as client:
+            res = client.get('/')
+
+            self.assertEqual(res.status_code, 200)
+
+    def test_show_convert_form_submit(self):
+        """Test"""
+
+        with app.test_client() as client:
+            # res = client.get('/')
+            res = client.get(
+                '/', query_string={'convert-from': 'usd', 'convert-to': 'sek', 'amount': '1'})
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            # check the query values
+            self.assertEqual(request.args['convert-from'], 'usd')
+            self.assertEqual(request.args['convert-to'], 'sek')
+            self.assertEqual(request.args['amount'], '1')
+            # check that the correct page is rendered
+            self.assertIn('<h1>Forex Converter</h1>', html)
+
+    def test_convert_currency(self):
+        """Test"""
+
+        with app.test_client() as client:
+            res = client.get(
+                '/convert', query_string={'convert-from': 'ron', 'convert-to': 'ron', 'amount': '1'})
+            self.assertEqual(request.args['convert-from'], 'ron')
+            self.assertEqual(request.args['convert-to'], 'ron')
+            self.assertEqual(res.status_code, 302)
+            self.assertEqual(
+                res.location, "http://localhost/conversion-result")
+
